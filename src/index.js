4ebfb3fe8ec23e9a6c40f5cbdb5e189ad40a451d
zhyshen <zhyshen@iastate.edu>
2023-01-27 23:42:27 -0600

    add dark mode detection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.querySelector("body").classList.add('night-mode');
+}
+
 ReactDOM.render(
   <App />, 
   document.getElementById("app")
